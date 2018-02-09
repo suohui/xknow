1899c6896af13a09df17d015d70c8ae4e3fbe898
1472723137 <1472723137@qq.com>
2018-02-09 17:07:25 +0800

    无边框窗体及其拖动

---
+++ XKnowCtrl/XKnowCtrl/MainDlg.h
@@ -0,0 +1,116 @@
+// MainDlg.h : interface of the CMainDlg class
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+//1、无边框窗体、可拖动
+//2、画LOGO、标题、背景
+//3、画窗体阴影
+//4、画按扭
+//5、画动画
+//6、画Label
+
+class CMainDlg : public CDialogImpl<CMainDlg>, public CUpdateUI<CMainDlg>,
+		public CMessageFilter, public CIdleHandler
+{
+public:
+	enum { IDD = IDD_MAINDLG };
+
+	virtual BOOL PreTranslateMessage(MSG* pMsg)
+	{
+		return CWindow::IsDialogMessage(pMsg);
+	}
+
+	virtual BOOL OnIdle()
+	{
+		UIUpdateChildWindows();
+		return FALSE;
+	}
+
+	BEGIN_UPDATE_UI_MAP(CMainDlg)
+	END_UPDATE_UI_MAP()
+
+	BEGIN_MSG_MAP(CMainDlg)
+		MESSAGE_HANDLER(WM_INITDIALOG, OnInitDialog)
+		MESSAGE_HANDLER(WM_DESTROY, OnDestroy)
+
+		MESSAGE_HANDLER(WM_NCHITTEST, OnNcHitTest)
+
+		COMMAND_ID_HANDLER(ID_APP_ABOUT, OnAppAbout)
+		COMMAND_ID_HANDLER(IDOK, OnOK)
+		COMMAND_ID_HANDLER(IDCANCEL, OnCancel)
+	END_MSG_MAP()
+
+// Handler prototypes (uncomment arguments if needed):
+//	LRESULT MessageHandler(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
+//	LRESULT CommandHandler(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+//	LRESULT NotifyHandler(int /*idCtrl*/, LPNMHDR /*pnmh*/, BOOL& /*bHandled*/)
+
+	LRESULT OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
+	{
+		// center the dialog on the screen
+		CenterWindow();
+
+		// set icons
+		HICON hIcon = AtlLoadIconImage(IDR_MAINFRAME, LR_DEFAULTCOLOR, ::GetSystemMetrics(SM_CXICON), ::GetSystemMetrics(SM_CYICON));
+		SetIcon(hIcon, TRUE);
+		HICON hIconSmall = AtlLoadIconImage(IDR_MAINFRAME, LR_DEFAULTCOLOR, ::GetSystemMetrics(SM_CXSMICON), ::GetSystemMetrics(SM_CYSMICON));
+		SetIcon(hIconSmall, FALSE);
+
+		// register object for message filtering and idle updates
+		CMessageLoop* pLoop = _Module.GetMessageLoop();
+		ATLASSERT(pLoop != NULL);
+		pLoop->AddMessageFilter(this);
+		pLoop->AddIdleHandler(this);
+
+		UIAddChildWindowContainer(m_hWnd);
+
+		SetWindowLong(GWL_STYLE, GetWindowLong(GWL_STYLE) & (~WS_BORDER) & WS_POPUP);//无边框窗体
+		SetWindowLong(GWL_EXSTYLE, GetWindowLong(GWL_EXSTYLE) | WS_EX_APPWINDOW);    //显示在任务栏
+
+		return TRUE;
+	}
+
+	LRESULT OnDestroy(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
+	{
+		// unregister message filtering and idle updates
+		CMessageLoop* pLoop = _Module.GetMessageLoop();
+		ATLASSERT(pLoop != NULL);
+		pLoop->RemoveMessageFilter(this);
+		pLoop->RemoveIdleHandler(this);
+
+		return 0;
+	}
+	//无边框窗体可拖动
+	LRESULT OnNcHitTest(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM lParam, BOOL& /*bHandled*/)
+	{
+		return HTCAPTION;
+	}
+
+	LRESULT OnAppAbout(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+	{
+		CAboutDlg dlg;
+		dlg.DoModal();
+		return 0;
+	}
+
+	LRESULT OnOK(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+	{
+		// TODO: Add validation code 
+		CloseDialog(wID);
+		return 0;
+	}
+
+	LRESULT OnCancel(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+	{
+		CloseDialog(wID);
+		return 0;
+	}
+
+	void CloseDialog(int nVal)
+	{
+		DestroyWindow();
+		::PostQuitMessage(nVal);
+	}
+};
---
+++ XKnowCtrl/XKnowCtrl/XKnowCtrl.h
@@ -0,0 +1,1 @@
+// XKnowCtrl.h
