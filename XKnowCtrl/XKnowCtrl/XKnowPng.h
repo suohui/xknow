564dbce81837f21e43f84582a911815909bf9247
suohui <350128260@qq.com>
2018-03-01 11:20:48 +0800

    功能已整合到CXKnowRend类中

--- XKnowCtrl/XKnowCtrl/XKnowPng.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-enum PNGTYPE
-{
-	TwoInOne,
-	ThreeInOne,
-	FourInOne
-};
-HBITMAP CreateHBitmapFromFile(LPCTSTR lpszFileName);
-HBITMAP CreateHBitmapFromFile(LPCTSTR lpszFileName, int &iWidth, int &iHeight, BOOL &bAlpha);
